cacc0ff310167b129829ee155c3dd136eecae3e0
Vasya Dotcom <vasssya.com@gmail.com>
2013-07-22 18:55:26 +0400

    Version 0.1.0

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ user.usd.update_attributes amount: 1000
 admin.btc.update_attributes amount: 100
 admin.usd.update_attributes amount: 1000
 
-Bid.create! user: user, buy_currency: :usd, sell_currency: :btc, amount: 300, max_price: 10
-Bid.create! user: admin, buy_currency: :btc, sell_currency: :usd, amount: 10, max_price: 300
-Bid.create! user: admin, buy_currency: :btc, sell_currency: :usd, amount: 10, max_price: 200
-Bid.create! user: admin, buy_currency: :btc, sell_currency: :usd, amount: 10, max_price: 400
+Bid.create! user: user, buy_currency: 'usd', sell_currency: 'btc', amount: 300, max_price: 10
+Bid.create! user: admin, buy_currency: 'btc', sell_currency: 'usd', amount: 10, max_price: 300
+Bid.create! user: admin, buy_currency: 'btc', sell_currency: 'usd', amount: 10, max_price: 200
+Bid.create! user: admin, buy_currency: 'btc', sell_currency: 'usd', amount: 10, max_price: 400
